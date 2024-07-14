2b11e885a8ff6fca5c3f92695685804c65b70040
Sam Wuu <iamkeason@gmail.com>
2024-07-14 13:47:30 +0800

    init

---
+++ src/data/resume.tsx
@@ -0,0 +1,467 @@
+import { Icons } from "@/components/icons";
+import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from "lucide-react";
+
+export const DATA = {
+  name: "Sam Wuu",
+  initials: "DV",
+  url: "https://sam5.pro",
+  location: "GD, China",
+  locationLink: "https://www.google.com/maps/place/guangdong",
+  description:
+    "2022 年底，我辞去了软件工程师的工作，全身心投入到建立和扩展自己的 SaaS 业务中，软件工程师转型为创造者。我喜欢创造事物并帮助他人。",
+  summary:
+    "2022 年底，我辞去了软件工程师的工作，全身心投入到建立和扩展自己的 SaaS 业务中。过去，我攻读了计算机科学和商业双学位，在硅谷的大型科技公司实习，并参加了 21 多次黑客马拉松比赛。我也很高兴成为 buildspace 有史以来第一个面对面的团队，名为 buildspace sf1。",
+  avatarUrl: "/icons/icon.svg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Go",
+    "Postgres",
+    "Docker",
+    "Kubernetes",
+    "Java",
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    { href: "#", icon: CodeIcon, label: "Projects" },
+    { href: "#", icon: PencilLine, label: "Notes" },
+  ],
+  contact: {
+    email: "hello@example.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://dub.sh/dillion-github",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://dub.sh/dillion-linkedin",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://dub.sh/dillion-twitter",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/dillion-youtube",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Atomic Finance",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "Bitcoin Protocol Engineer",
+      logoUrl: "/atomic.png",
+      start: "May 2021",
+      end: "Oct 2022",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+    {
+      company: "Shopify",
+      badges: [],
+      href: "https://shopify.com",
+      location: "Remote",
+      title: "Software Engineer",
+      logoUrl: "/shopify.svg",
+      start: "January 2021",
+      end: "April 2021",
+      description:
+        "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+    },
+    {
+      company: "Nvidia",
+      href: "https://nvidia.com/",
+      badges: [],
+      location: "Santa Clara, CA",
+      title: "Software Engineer",
+      logoUrl: "/nvidia.png",
+      start: "January 2020",
+      end: "April 2020",
+      description:
+        "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
+    },
+    {
+      company: "Splunk",
+      href: "https://splunk.com",
+      badges: [],
+      location: "San Jose, CA",
+      title: "Software Engineer",
+      logoUrl: "/splunk.svg",
+      start: "January 2019",
+      end: "April 2019",
+      description:
+        "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
+    },
+    {
+      company: "Lime",
+      href: "https://li.me/",
+      badges: [],
+      location: "San Francisco, CA",
+      title: "Software Engineer",
+      logoUrl: "/lime.svg",
+      start: "January 2018",
+      end: "April 2018",
+      description:
+        "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
+    },
+    {
+      company: "Mitre Media",
+      href: "https://mitremedia.com/",
+      badges: [],
+      location: "Toronto, ON",
+      title: "Software Engineer",
+      logoUrl: "/mitremedia.png",
+      start: "May 2017",
+      end: "August 2017",
+      description:
+        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    },
+  ],
+  education: [
+    {
+      school: "Buildspace",
+      href: "https://buildspace.so",
+      degree: "s3, s4, sf1, s5",
+      logoUrl: "/buildspace.jpg",
+      start: "2023",
+      end: "2024",
+    },
+    {
+      school: "University of Waterloo",
+      href: "https://uwaterloo.ca",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/waterloo.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "Wilfrid Laurier University",
+      href: "https://wlu.ca",
+      degree: "Bachelor's Degree of Business Administration (BBA)",
+      logoUrl: "/laurier.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "International Baccalaureate",
+      href: "https://ibo.org",
+      degree: "IB Diploma",
+      logoUrl: "/ib.png",
+      start: "2012",
+      end: "2016",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+  hackathons: [
+     
+    {
+      title: "HackDavis",
+      dates: "January 20th - 21st, 2018",
+      location: "Davis, California",
+      description:
+        "Developed a mobile application which allocates a daily carbon emission allowance to users to move towards a sustainable environment.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-davis.png",
+      win: "Best Data Hack",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2018/white.svg",
+      links: [
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/my6footprint",
+        },
+        {
+          title: "ML",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/my6footprint-machine-learning",
+        },
+        {
+          title: "iOS",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/CarbonWallet",
+        },
+        {
+          title: "Server",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Wallet6/wallet6-server",
+        },
+      ],
+    },
+    
+    {
+      title: "Hack The 6ix",
+      dates: "August 26th - 27th, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed an open platform for people shipping items to same place to combine shipping costs and save money.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-6ix.jpg",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/ShareShip/ShareShip",
+        },
+        {
+          title: "Site",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://share-ship.herokuapp.com/",
+        },
+      ],
+    },
+    {
+      title: "Stupid Hack Toronto",
+      dates: "July 23rd, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed a chrome extension which tracks which facebook profiles you have visited and immediately texts your girlfriend if you visited another girls page.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/stupid-hackathon.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/nsagirlfriend/nsagirlfriend",
+        },
+      ],
+    },
+    
+    {
+      title: "Open Source Circular Economy Days Hackathon",
+      dates: "June 10th, 2017",
+      location: "Toronto, Ontario",
+      description:
+        "Developed a custom admin interface for food waste startup <a href='http://genecis.co/'>Genecis</a> to manage their data and provide analytics.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/open-source-circular-economy-days.jpg",
+      win: "1st Place Winner",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/genecis",
+        },
+      ],
+    },
+    {
+      title: "Make School's Student App Competition 2017",
+      dates: "May 19th - 21st, 2017",
+      location: "International",
+      description: "Improved PocketDoc and submitted to online competition",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/make-school-hackathon.png",
+      win: "Top 10 Finalist | Honourable Mention",
+      links: [
+        {
+          title: "Medium Article",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://medium.com/make-school/the-winners-of-make-schools-student-app-competition-2017-a6b0e72f190a",
+        },
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+    {
+      title: "HackMining",
+      dates: "May 12th - 14th, 2017",
+      location: "Toronto, Ontario",
+      description: "Developed neural network to optimize a mining process",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-mining.png",
+      links: [],
+    },
+    {
+      title: "Waterloo Equithon",
+      dates: "May 5th - 7th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed Pocketdoc, an app in which you take a picture of a physical wound, and the app returns common solutions or cures to the injuries or diseases.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/waterloo-equithon.png",
+      links: [
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+     
+  ],
+} as const;
